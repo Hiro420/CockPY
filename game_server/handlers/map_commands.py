da06accadca2a9182b35cc4cb8d36428da98d22a
NickTheHuy <nikurlapov@gmail.com>
2023-03-07 23:38:26 +0200

    implement enter scene with command

--- game_server/handlers/map_commands.py
+++ game_server/handlers/map_commands.py
@@ -7,6 +7,8 @@ from random import randrange
 import json
 from os import path
 import time
+import re
+import os
 
 basepath = path.dirname(__file__)
 hpcalcsname = "Monster_HP_calcs_lv90.json"
@@ -67,13 +69,51 @@ def handle_map_tp(conn: Connection, msg: MarkMapReq):
                 except:
                     scene_id = msg.mark.scene_id
                     
-            pos = Vector(0, 500, 0)
+            #pos = Vector(0, 500, 0)
             
-            conn.send(conn.player.get_teleport_packet(scene_id, pos, EnterType.ENTER_JUMP))
-            
+            lua_file_pre = os.path.abspath(f'.\\game_server\\lua\\scene{scene_id}.lua')
+            print(lua_file_pre)
+            #lua_file_pre = f"C:\\Users\\nikur\\Documents\\CockPY\\game_server\\lua\\scene{scene_id}.lua"
+            lua_file = open(lua_file_pre, encoding='utf8')
+            global player_x
+            global player_y
+            global player_z
+            player_x = conn.player.pos.x
+            player_y = conn.player.pos.y
+            player_z = conn.player.pos.z
+
+            print(f"Current position on map: X = {player_x}, Y = {player_y}, Z = {player_z}")
+
+            #print("Contents of lua_file:")
+
+            lines = lua_file.readlines()
+            for line in lines:
+                if "born_pos" in line:
+                    fline = (line[1 : -2])
+                    #print(line[1:-2])
+
+            match = re.search(r'born_pos = { x = ([\d\.-]+), y = ([\d\.-]+), z = ([\d\.-]+) }', fline)
+
+            if match:
+                born_pos_x, born_pos_y, born_pos_z = match.groups()
+                born_pos_x = float(match.group(1))
+                born_pos_y = float(match.group(2))
+                born_pos_z = float(match.group(3))
+                print(f"Attemptint to enter scene {scene_id} in positions: X = {born_pos_x}, Y = {born_pos_y}, Z = {born_pos_z}")
+                #born_pos = f"{(match.group(1), match.group(2), match.group(3))}"
+                pos = Vector(born_pos_x, born_pos_y, born_pos_z)
+                #print(f"{pos}")
+            else:
+                print("Values not found in line")
+                pos = Vector(0, 0, 0)
+                rot = Vector(0, 0, 0)
+
+            print(scene_id)
+        
             conn.player.pos = pos
-            conn.player.scene_id = scene_id
-            conn.player.get_cur_avatar().motion = pos
+
+            conn.send(conn.player.get_teleport_packet(scene_id, pos, EnterType.ENTER_DUNGEON))
+
         elif msg.mark:
             if msg.mark.point_type == 3:
                 global cg_id
