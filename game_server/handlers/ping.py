be2822d487961b273b007d16e7712cbd92801770
YYLMZXC <90677130+YYLMZXC@users.noreply.github.com>
2023-03-20 02:42:53 +0800

    修复在Linux上运行无法找到文件路径的bug
    修复在Linux上运行无法找到文件路径的bug

--- game_server/handlers/ping.py
+++ game_server/handlers/ping.py
@@ -1,6 +1,6 @@
 from game_server.protocol.cmd_id import CmdID
-from game_server import HandlerRouter,Connection
-from lib.proto import PingReq,PingRsp,PlayerLuaShellNotify
+from game_server import HandlerRouter, Connection
+from lib.proto import PingReq, PingRsp, PlayerLuaShellNotify
 import enet
 import os
 
@@ -12,14 +12,13 @@ def handle_ping(conn: Connection, msg: PingReq):
     rsp.client_time = msg.client_time
     rsp.seq = msg.seq
     rsp1 = PlayerLuaShellNotify()
-    rsp1.id=1
+    rsp1.id = 1
     rsp2 = PlayerLuaShellNotify()
-    rsp2.id=1
-    file = open(f'{os.path.realpath}\\..\\Windy\\script.lua',"rb")
-    file2 = open(f'{os.path.realpath}\\..\\Windy\\fps.lua',"rb")
-    rsp1.lua_shell = file.read()
-    rsp2.lua_shell = file2.read()
-    file.close()
+    rsp2.id = 1
+    with open('Windy/script.lua', 'rb') as file:
+        rsp1.lua_shell = file.read()
+    with open('Windy/fps.lua', 'rb') as file2:
+        rsp2.lua_shell = file2.read()
     conn.send(rsp)
     conn.send(rsp1)
     conn.send(rsp2)
