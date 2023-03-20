74a87e2352a06299e57c8c13ee48e30d282df59a
NickTheHuy <nikurlapov@gmail.com>
2023-03-20 14:52:43 +0200

    another goofy mistake i did

--- game_server/handlers/ping.py
+++ game_server/handlers/ping.py
@@ -5,21 +5,19 @@ import enet
 import os
 
 router = HandlerRouter()
+base_script_path = os.path.join(os.path.realpath('.'), "Windy")
 
 @router(CmdID.PingReq)
 def handle_ping(conn: Connection, msg: PingReq):
     rsp = PingRsp()
     rsp.client_time = msg.client_time
     rsp.seq = msg.seq
-    rsp1 = PlayerLuaShellNotify()
-    rsp1.id=1
-    rsp2 = PlayerLuaShellNotify()
-    rsp2.id=1
-    file = open(f'{os.path.realpath}\\..\\Windy\\script.lua',"rb")
-    file2 = open(f'{os.path.realpath}\\..\\Windy\\fps.lua',"rb")
-    rsp1.lua_shell = file.read()
-    rsp2.lua_shell = file2.read()
-    file.close()
+
     conn.send(rsp)
-    conn.send(rsp1)
-    conn.send(rsp2)
+    
+    for script in ("script.lua", "fps.lua"):
+        with open(os.path.join(base_script_path, script), "rb") as f:
+            lua = PlayerLuaShellNotify()
+            lua.id = 1
+            lua.lua_shell = f.read()
+            conn.send(lua)
